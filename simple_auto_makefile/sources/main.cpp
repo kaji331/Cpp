ed83f69ec8489c3837cbc5240d86f1fa0192075a
kaji331 <kaji331@hotmail.com>
2014-07-31 00:02:05 +0800

    小玩玩

--- simple_auto_makefile/sources/main.cpp
+++ simple_auto_makefile/sources/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
 	cout << "Hello World!" << endl;
+
+	vector<int> data;
+	vector<int>::iterator pos;
+
+	for (int i = 0; i <= 10; i++)
+		data.push_back(i);
+
+	for (pos = data.begin(); pos != data.end(); pos++)
+		cout << *pos <<" ";
+	cout << endl;
+
 	return 0;
 }
